72c94b8d083380296aab781de991ac9007e26b6c
ChoiIseungil <silly5921@kaist.ac.kr>
2022-06-10 23:52:21 +0200

    csv added

--- cifar.py
+++ cifar.py
@@ -1,6 +1,7 @@
+from logging import raiseExceptions
 import torch
 import torchvision
-from models import AlexNet
+from models import AlexNet, ResNet18
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.datasets import CIFAR10
@@ -9,18 +10,19 @@ from torchvision.utils import make_grid
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data import random_split, Subset
 from torchvision import transforms
-from torchvision.utils import save_image
 import torch.optim as optim
 import torch.nn as nn
+import csv
 
 import os
 import argparse
-from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='CS439 Experiment, by Seungil Lee')
 parser.add_argument('--', '-r', action='store_true',
                     help='resume from checkpoint')
+parser.add_argument('-epoch', '-e', default = 200, type = int,
+                    help='number of epochs to be trained')
 parser.add_argument('-trainsize', '-t', default = 2**15, type = int,
                     help='size of trainset')
 parser.add_argument('-batchratio', '-b', default = 2**11, type = int,
@@ -29,9 +31,12 @@ parser.add_argument('-model', '-m', required = True,
                     help='Choose between ResNet and AlexNet')
 args = parser.parse_args()
 
+MODEL = args.model
+BATCHRATIO = args.batchratio
 TRAINSIZE = args.trainsize
 VALSIZE = int(TRAINSIZE/3)
-BATCHSIZE = int(TRAINSIZE/args.batchratio)
+BATCHSIZE = int(TRAINSIZE/BATCHRATIO)
+
 
 def main():
     #creating a dinstinct transform class for the train, validation and test dataset
@@ -60,24 +65,45 @@ def main():
 
     ## Loss and optimizer
     learning_rate = 1e-4
-    load_model = True
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr= learning_rate) #Adam seems to be the most popular for deep learning
 
-    device = torch.device('cuda')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        raise NotImplementedError
 
-    model = AlexNet() #to compile the model
-    model = model.to(device=device) #to send the model for training on either cuda or cpu
+    if MODEL == 'AlexNet':
+        model = AlexNet()
+    elif MODEL == 'ResNet':
+        model = ResNet18()
+    model = model.to(device=device)
 
     epoch_values = []
-    loss_values = []
+    trainloss_values = []
+    valloss_values = []
     acc_values = []
+    test_value = []
+
+    def save_model(model):
+        if not os.path.isdir('saved'):
+            os.mkdir('saved')
+        torch.save(model.state_dict(), f"saved/{MODEL}_cifar_{BATCHSIZE}:{BATCHRATIO}.pt")
+        print(f"saved/{MODEL}_cifar_{BATCHSIZE}:{BATCHRATIO}.pt saved!")
+
+    def save_csv():
+        if not os.path.isdir('log'):
+            os.mkdir('log')
+        with open(f"./log/saved/{MODEL}_cifar_{BATCHSIZE}:{BATCHRATIO}.csv", 'w') as f:
+            write = csv.writer(f)
+            write.writerow(["Epoch","Train Loss","Validation Loss","Accuracy","Result"])
+            write.writerow([epoch_values, trainloss_values, valloss_values, acc_values, test_value])
+
 
     def train(epoch):
+        best_acc = 0.
         print(f'\nEpoch {epoch}')
         model.train()
-        loss_ep = 0
-        
         for batch_idx, (data, targets) in enumerate(trainloader):
             data = data.to(device=device)
             targets = targets.to(device=device)
@@ -86,58 +112,70 @@ def main():
             loss = criterion(scores,targets)
             loss.backward()
             optimizer.step()
-
-            train_loss += loss.item()
-            _, predicted = scores.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            epoch_values.append(epoch)
-            loss_values.append(train_loss/(batch_idx+1))
-            acc_values.append(100.*correct/total)
-
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                        % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-    def test(epoch):
-        global best_acc
-        model.eval()
-        test_loss = 0
+            loss_ep += loss.item()
+            
+        train_loss = loss_ep/len(trainloader)
+            
         with torch.no_grad():
             num_correct = 0
             num_samples = 0
-            for _, (data,targets) in enumerate(valloader):
+            for batch_idx, (data,targets) in enumerate(valloader):
                 data = data.to(device=device)
                 targets = targets.to(device=device)
                 scores = model(data)
+                val_loss = criterion(scores,targets)
+
                 _, predictions = scores.max(1)
                 num_correct += (predictions == targets).sum()
                 num_samples += predictions.size(0)
-            acc = float(num_correct) / float(num_samples) * 100
-            print(
-                f"Got {num_correct} / {num_samples} with accuracy {acc:.2f}"
-            )
-        acc_values.append(acc)
+                acc = 100.*(num_correct/num_samples)
 
+                print(
+                    f"Epoch {epoch}:: Train Loss {train_loss:.4f}, Validation Loss {val_loss:.4f}, Accuracy {acc:.2f} ({num_correct} / {num_samples})"
+                )
+                
+                epoch_values.append(epoch)
+                trainloss_values.append(train_loss)
+                valloss_values.append(val_loss)
+                acc_values.append(acc)
+        
+        if acc > best_acc:
+            save_model(model)
+            best_acc = acc
 
-    torch.save(model.state_dict(), f"saved/alexnet_cifar_{BATCHSIZE}:{BATCHRATIO}.pt") #SAVES THE TRAINED MODEL
+    def test():
+        if MODEL == 'AlexNet':
+            model = AlexNet()
+        elif MODEL == 'ResNet':
+            model = ResNet18()
 
-    num_correct = 0
-    num_samples = 0
-    for _, (data,targets) in enumerate(testloader):
-        data = data.to(device=device)
-        targets = targets.to(device=device)
-        scores = model(data)
-        _, predictions = scores.max(1)
-        num_correct += (predictions == targets).sum()
-        num_samples += predictions.size(0)
-    print(
-        f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}"
-    )
+        model.load_state_dict(torch.load("saved/{MODEL}_cifar_{BATCHSIZE}:{BATCHRATIO}.pt")) #loads the trained model
+        model = model.to(device=device) #to send the model for training on either cuda or cpu
+        model.eval()
 
-    for epoch in range(200):
+        with torch.no_grad():
+            num_correct = 0
+            num_samples = 0
+            for batch_idx, (data,targets) in enumerate(testloader):
+                data = data.to(device=device)
+                targets = targets.to(device=device)
+                scores = model(data)
+
+                _, predictions = scores.max(1)
+                num_correct += (predictions == targets).sum()
+                num_samples += predictions.size(0)
+                acc = float(num_correct) / float(num_samples) * 100
+
+                print("Testing...")
+                print(
+                    f"Got {num_correct} / {num_samples} with accuracy {acc:.2f}"
+                )
+                test_values.append(acc)
+
+    for epoch in range(args.epoch):
         train(epoch)
-        test(epoch)
+    save_csv()
+    test()
 
 if __name__ == '__main__':
     main()
