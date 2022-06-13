a523a5031565820dda246c2854a1acd2d819c426
ChoiIseungil <silly5921@kaist.ac.kr>
2022-06-13 03:01:29 +0200

    change run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**3
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**4
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**5
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**6
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**7
-python cifar.py -g 2 -m AlexNet -t 2**15 -b 2**8
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**3
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**4
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**5
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**6
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**7
+python main.py -g 2 -m AlexNet -t 2**15 -b 2**8
 
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**3
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**4
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**5
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**6
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**7
-python cifar.py -g 2 -m AlexNet -t 2**14 -b 2**8
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**3
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**4
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**5
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**6
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**7
+python main.py -g 2 -m AlexNet -t 2**14 -b 2**8
 
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**3
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**4
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**5
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**6
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**7
-python cifar.py -g 2 -m AlexNet -t 2**13 -b 2**8
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**3
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**4
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**5
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**6
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**7
+python main.py -g 2 -m AlexNet -t 2**13 -b 2**8
