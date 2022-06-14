d7eb3b91613f78cff639e8d62f2540645e0625e5
ChoiIseungil <silly5921@kaist.ac.kr>
2022-06-14 02:00:22 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 3
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 4
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 5
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 6
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 7
-python main.py -g 2 -d cifar -m AlexNet -t 15 -b 8
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 3
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 4
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 5
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 6
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 7
+python main.py -g 2 -d cifar -m ResNet -t 15 -b 8
 
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 3
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 4
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 5
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 6
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 7
-python main.py -g 2 -d cifar -m AlexNet -t 14 -b 8
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 3
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 4
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 5
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 6
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 7
+python main.py -g 2 -d cifar -m ResNet -t 14 -b 8
 
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 3
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 4
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 5
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 6
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 7
-python main.py -g 2 -d cifar -m AlexNet -t 13 -b 8
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 3
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 4
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 5
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 6
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 7
+python main.py -g 2 -d cifar -m ResNet -t 13 -b 8
