62ea581fcb15e0b1cf2c4226b9097e65a62547d0
ChoiIseungil <silly5921@kaist.ac.kr>
2022-06-14 02:33:37 +0200

    VGG 13~15 cifar

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 3
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 4
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 5
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 6
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 7
-python main.py -g 2 -d cifar -m ResNet -t 12 -b 8
+python main.py -g 2 -d cifar -m VGG -t 15 -b 3
+python main.py -g 2 -d cifar -m VGG -t 15 -b 4
+python main.py -g 2 -d cifar -m VGG -t 15 -b 5
+python main.py -g 2 -d cifar -m VGG -t 15 -b 6
+python main.py -g 2 -d cifar -m VGG -t 15 -b 7
+python main.py -g 2 -d cifar -m VGG -t 15 -b 8
 
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 3
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 4
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 5
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 6
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 7
-python main.py -g 2 -d cifar -m ResNet -t 11 -b 8
+python main.py -g 2 -d cifar -m VGG -t 14 -b 3
+python main.py -g 2 -d cifar -m VGG -t 14 -b 4
+python main.py -g 2 -d cifar -m VGG -t 14 -b 5
+python main.py -g 2 -d cifar -m VGG -t 14 -b 6
+python main.py -g 2 -d cifar -m VGG -t 14 -b 7
+python main.py -g 2 -d cifar -m VGG -t 14 -b 8
 
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 3
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 4
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 5
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 6
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 7
-python main.py -g 2 -d cifar -m ResNet -t 10 -b 8
+python main.py -g 2 -d cifar -m VGG -t 13 -b 3
+python main.py -g 2 -d cifar -m VGG -t 13 -b 4
+python main.py -g 2 -d cifar -m VGG -t 13 -b 5
+python main.py -g 2 -d cifar -m VGG -t 13 -b 6
+python main.py -g 2 -d cifar -m VGG -t 13 -b 7
+python main.py -g 2 -d cifar -m VGG -t 13 -b 8
